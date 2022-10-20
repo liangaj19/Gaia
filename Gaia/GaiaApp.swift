4a438babb68128ba8d087610d5bbb14f97ce90fd
Amy Liang <aliang1903@gmail.com>
2022-10-20 18:14:37 -0400

    add project folder

---
+++ Gaia/GaiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaiaApp.swift
+//  Gaia
+//
+//  Created by Amy Liang on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct GaiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
