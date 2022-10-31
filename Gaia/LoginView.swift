f784dcb1cd0bf7066266e51b4f5b37132bed5497
Amy Liang <aliang1903@gmail.com>
2022-10-31 19:21:42 -0400

    basic textfield and stuff

--- Gaia/LoginView.swift
+++ Gaia/LoginView.swift
@@ -18,24 +18,59 @@ struct LoginView: View {
             NavigationStack {
                 VStack {
                     Text("Login")
+                    
+                    //enter username
                     TextField("Username", text: $username)
                         .autocapitalization(.none)
                         .padding(.all)
                         .frame(width: 300, height: 50, alignment: .center)
                         .background(Color.gray)
                         .cornerRadius(10)
+                    
+                    //enter password
                     TextField("Password", text: $password)
                         .autocapitalization(.none)
                         .padding(.all)
                         .frame(width: 300, height: 50, alignment: .center)
                         .background(Color.gray)
                         .cornerRadius(10)
-                    Text("Not a user yet?")
+                    
+                    Spacer()
+                        .frame(height: 50)
+                    
+                    Group {
+                        Text("Not a user yet?")
+                        
+                        //create an account button
+                        NavigationLink {
+                            CreateView()
+                        } label: {
+                            Text("Create an account")
+                                .frame(width: 275, height: 30, alignment: .center)
+                                .foregroundColor(Color.white)
+                                .padding()
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 40)
+                                        .stroke(Color.blue, lineWidth: 3)
+                                )
+                                .background(Color.black)
+                                .cornerRadius(40)
+                        }
+                        
+                        //use as guest button
+                        NavigationLink {
+                            SearchView()
+                        } label: {
+                            Text("Continue as guest")
+                        }
+                    }
+                    .offset(y:200)
                 }
             }
         }
     }
 }
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
