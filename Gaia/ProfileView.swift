4baf98fc7a199ee12e2fa86fa6c6fc4618b5aac3
Amy Liang <aliang1903@gmail.com>
2022-11-19 16:37:34 -0500

    yeet

--- Gaia/ProfileView.swift
+++ Gaia/ProfileView.swift
@@ -9,80 +9,28 @@ import SwiftUI
 
 struct ProfileView: View {
     
-    @State private var username: String = "Guest"
-    
-    @State var currentTab = "Allergens"
-    
     var body: some View {
-        ZStack {
-            VStack {
-                ScrollView(.vertical, showsIndicators: false) {
+        VStack {
+            ZStack (alignment: .bottomLeading) {
+                //banner
+                Color("pearlyPurple")
+                    .ignoresSafeArea()
+                
+                //profile picture
+                Image("samplePFP")
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 100, height: 100)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.black)
+                    )
+                    .offset(x: 20, y: 50)
                     
-                    // banner and pfp
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(Color("matcha"))
-                            .frame(maxWidth: .infinity, maxHeight: 200)
-                            .ignoresSafeArea(.all)
-                        
-                        HStack {
-                            Image("samplePFP")
-                                .resizable()
-                                .frame(width:130, height: 130, alignment: .center)
-                                .clipShape(Circle())
-                                .padding()
-                                .overlay(
-                                    Circle()
-                                        .stroke(Color("matcha"), lineWidth: 3)
-                                        .frame(width: 130, height: 130, alignment: .center)
-                                )
-                                .shadow(radius: 10)
-                                .offset(y:90)
-                            
-                            Text(username)
-                                .font(.system(size: 22, weight: .bold))
-                                .frame(minWidth: 200, alignment: .leading)
-                                .offset(y: 115)
-                        }
-                    }
-                    
-                    // information
-                    VStack {
-                        
-                        // edit profile button
-                        HStack {
-                            Spacer()
-                                .frame(width: 220)
-                            
-                            Menu {
-                                Button(action: {
-                                    //
-                                }) {
-                                    Text("Edit profile")
-                                }
-                                
-                                Button (action: {
-                                    
-                                }) {
-                                    Text("Add an allergen")
-                                }
-                            } label: {
-                                Image(systemName: "ellipsis")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .foregroundColor(Color.white)
-                                    .frame(width: 40, height: 80)
-                            }
-                            .offset(x: 30, y:-185)
-                            
-                        }
-                        
-                    }
-                    .offset(y:40)
-                    
-                }
-                .ignoresSafeArea(.all)
             }
+            .frame(height: 100)
+            
+            Spacer()
         }
     }
 }
