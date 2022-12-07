5ced8230c760891a7bef8653142f2bcd2329ee21
Amy Liang <aliang1903@gmail.com>
2022-12-07 18:43:43 -0500

    pog

--- Gaia/WelcomeView.swift
+++ Gaia/WelcomeView.swift
@@ -22,95 +22,107 @@ struct WelcomeView: View {
          * if the welcome screen showed for 3 seconds,
          * it'll go to the login view
          */
-        if isActive && firstTimeUser == true {
+        if isActive == false && firstTimeUser == true {
+            welcomeScreen
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                        self.isActive = true
+                    }
+                    UserDefaults.standard.welcomeScreenShown = false
+                }
             AllergenPickView()
         } else {
-            ZStack {
-                // circles in the background
-                Group {
-                    // pink circles
-                    Circle()
-                        .foregroundColor(Color("lightPink"))
-                        .frame(width: 240, height: 240)
-                        .offset(x: -170, y: 390)
-                    
-                    // pearly purple
-                    Circle()
-                        .foregroundColor(Color("pearlyPurple"))
-                        .frame(width: 100, height: 100)
-                        .offset(x: -200, y: 120)
-                    Circle()
-                        .foregroundColor(Color("pearlyPurple"))
-                        .frame(width: 240, height: 240)
-                        .offset(x: 190, y: -480)
-                    
-                    // blue circles
-                    Circle()
-                        .foregroundColor(Color("beauBlue"))
-                        .frame(width: 220, height: 220)
-                        .offset(x: 220, y: 150)
-                    Circle()
-                        .foregroundColor(Color("beauBlue"))
-                        .frame(width: 190, height: 190)
-                        .offset(x: -220, y: -320)
-                    
-                    // green circles
-                    Circle()
-                        .foregroundColor(Color("matcha"))
-                        .frame(width: 115, height: 120)
-                        .offset(x: 100, y: 420)
-                    Circle()
-                        .foregroundColor(Color("matcha"))
-                        .frame(width: 100, height: 100)
-                        .offset(x: 200, y: -180)
-                    
-                    // yellow circle
-                    Circle()
-                        .foregroundColor(Color("lightYellow"))
-                        .frame(width: 115, height: 115)
-                        .offset(x: -220, y: -100)
-                }
-                
-                VStack {
-                    
-                    VStack (spacing: 0) {
-                        Image("smallIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 290, height: 290, alignment: .center)
-                            .offset(y:30)
-                        
-                        Image("logo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200, alignment: .center)
-                            .offset(y: -120)
+            welcomeScreen
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                        self.isActive = true
                     }
-                    .offset(y:15)
-                    
-                    // Editing the whole screen and making it disappear after this first time
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    // .background(Color.white)
-                    .onAppear(perform: {
-                        UserDefaults.standard.welcomeScreenShown = true
-                    })
+                    UserDefaults.standard.welcomeScreenShown = false
                 }
-            }
-            // so the welcome screen only shows for 3 seconds
-            
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                    self.isActive = true
-                }
-                UserDefaults.standard.welcomeScreenShown = false
-            }
-             
+            MainView()
         }
     }
 }
 
+
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
         WelcomeView()
+    }
+}
+
+extension WelcomeView {
+    var welcomeScreen : some View {
+        
+        ZStack {
+            // circles in the background
+            Group {
+                // pink circles
+                Circle()
+                    .foregroundColor(Color("lightPink"))
+                    .frame(width: 240, height: 240)
+                    .offset(x: -170, y: 390)
+                
+                // pearly purple
+                Circle()
+                    .foregroundColor(Color("pearlyPurple"))
+                    .frame(width: 100, height: 100)
+                    .offset(x: -200, y: 120)
+                Circle()
+                    .foregroundColor(Color("pearlyPurple"))
+                    .frame(width: 240, height: 240)
+                    .offset(x: 190, y: -480)
+                
+                // blue circles
+                Circle()
+                    .foregroundColor(Color("beauBlue"))
+                    .frame(width: 220, height: 220)
+                    .offset(x: 220, y: 150)
+                Circle()
+                    .foregroundColor(Color("beauBlue"))
+                    .frame(width: 190, height: 190)
+                    .offset(x: -220, y: -320)
+                
+                // green circles
+                Circle()
+                    .foregroundColor(Color("matcha"))
+                    .frame(width: 115, height: 120)
+                    .offset(x: 100, y: 420)
+                Circle()
+                    .foregroundColor(Color("matcha"))
+                    .frame(width: 100, height: 100)
+                    .offset(x: 200, y: -180)
+                
+                // yellow circle
+                Circle()
+                    .foregroundColor(Color("lightYellow"))
+                    .frame(width: 115, height: 115)
+                    .offset(x: -220, y: -100)
+            }
+            
+            VStack {
+                
+                VStack (spacing: 0) {
+                    Image("smallIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 290, height: 290, alignment: .center)
+                        .offset(y:30)
+                    
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .offset(y: -120)
+                }
+                .offset(y:15)
+                // Editing the whole screen and making it disappear after this first time
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                // .background(Color.white)
+                .onAppear(perform: {
+                    UserDefaults.standard.welcomeScreenShown = true
+                })
+            }
+        }
     }
 }
