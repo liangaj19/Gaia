dae8b44ce207b39c7bda8c2eaba6d62d370d97ed
Anna Mei <amcode16@gmail.com>
2022-12-07 18:48:14 -0600

    barcode

--- Gaia/Barcode/AppViewModel.swift
+++ Gaia/Barcode/AppViewModel.swift
@@ -1,8 +1,81 @@
-//
-//  AppViewModel.swift
-//  Gaia
-//
-//  Created by student on 12/7/22.
-//
-
+import AVKit
 import Foundation
+import SwiftUI
+import VisionKit
+
+enum ScanType: String {
+    case barcode
+}
+
+enum DataScannerAccessStatusType {
+    case notDetermined // once user launches app
+    case cameraAccessNotGranted // when user does not give access to camera
+    case cameraNotAvailable // no camera on device
+    case scannerAvailable // scanner available
+    case scannerNotAvailable // scanner not available
+}
+
+@MainActor
+final class AppViewModel: ObservableObject {
+    
+    @Published var dataScannerAccessStatus: DataScannerAccessStatusType = .notDetermined
+    @Published var recognizedItems: [RecognizedItem] = [] // puts items in an array to be displayed on application
+    @Published var scanType: ScanType = .barcode // barcode default
+    @Published var textContentType: DataScannerViewController.TextContentType?  //text scanning is second optional
+    @Published var recognizesMultipleItems = true
+    
+    var recognizedDataType: DataScannerViewController.RecognizedDataType {
+        scanType == .barcode ? .barcode() : .text(textContentType: textContentType)
+    }
+    
+    var headerText: String {
+        if recognizedItems.isEmpty {
+            return "Scanning \(scanType.rawValue)"
+        } else {
+            return "Recognized \(recognizedItems.count) item(s)"
+        }
+    }
+    
+      var dataScannerViewId: Int {
+        var hasher = Hasher()
+        hasher.combine(scanType)
+        hasher.combine(recognizesMultipleItems)
+        if let textContentType {
+            hasher.combine(textContentType)
+        }
+        return hasher.finalize()
+    }
+    
+    private var isScannerAvailable: Bool {
+        DataScannerViewController.isAvailable && DataScannerViewController.isSupported  //returns true or false if both camera is available and supported
+    }
+    
+    func requestDataScannerAccessStatus() async {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            dataScannerAccessStatus = .cameraNotAvailable  //check for camera hardware
+            return
+        }
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            
+        case .authorized:
+            dataScannerAccessStatus = isScannerAvailable ? .scannerAvailable : .scannerNotAvailable
+            
+        case .restricted, .denied:
+            dataScannerAccessStatus = .cameraAccessNotGranted  //when users do not grant access to use camera
+            
+        case .notDetermined:
+            let granted = await AVCaptureDevice.requestAccess(for: .video)
+            if granted {
+                dataScannerAccessStatus = isScannerAvailable ? .scannerAvailable : .scannerNotAvailable
+            } else {
+                dataScannerAccessStatus = .cameraAccessNotGranted
+            }
+        
+        default: break
+            
+        }
+    }
+    
+    
+}
--- Gaia/Barcode/BarcodeView.swift
+++ Gaia/Barcode/BarcodeView.swift
@@ -1,54 +1,96 @@
-//
-//  FoodAllergyView.swift
-//  Gaia
-//
-//  Created by Geovanni Tinoco on 10/24/22.
-//
-
-
-// USE THIS LINK to connect to the FOOD ALLERGY VIEW for future tabs
-//NavigationLink(destination: FoodAllergyView(allergyListViewModel: AllergyListViewModel())) {
-//                    Text("Go To Add")
-//                        .foregroundColor(Color.white)
-                    
-//                        .bold()
- //               }
-//.buttonStyle(.borderedProminent)
-//.navigationTitle("Green")
-//.navigationBarTitleDisplayMode(.inline)
-
-
 import SwiftUI
+import VisionKit
 
-//This file is a view that shows a button called "Add" and will add a blank "name" value into the firebase database when you push it
 struct BarcodeView: View {
-    @ObservedObject var allergyListViewModel: AllergyListViewModel
+    
+    @EnvironmentObject var vm: AppViewModel
+    
     var body: some View {
-        ZStack{
-            Color.white
-            VStack{
-                Text("Scan Your Allergy:")
-                Button("Scan", action: addAllergy)
-                    .font(.title)
-                    .foregroundColor(Color.green)
+        switch vm.dataScannerAccessStatus {
+        case .scannerAvailable:
+            mainView
+        case .cameraNotAvailable:
+            Text("Device Does Not Have A Camera")
+        case .scannerNotAvailable:
+            Text("Device Is Not Supported For Scanning Barcode With This App")
+        case .cameraAccessNotGranted:
+            Text("Camera Access Previously Denied, Please Vist Camera Setting To Provide Access")
+        case .notDetermined:
+            Text("Requesting Camera Access")
+        }
+    }
+    
+    private var mainView: some View {
+        DataScannerView(
+            recognizedItems: $vm.recognizedItems,
+            recognizedDataType: vm.recognizedDataType,
+            recognizesMultipleItems: vm.recognizesMultipleItems)
+        .background { Color.gray.opacity(0.3) } // darkness background to give contrast when barcode is available
+        .ignoresSafeArea()
+        .id(vm.dataScannerViewId)
+        .sheet(isPresented: .constant(true)) { // dark space under to show information
+            bottomContainerView
+                .background(.ultraThinMaterial)
+                .presentationDetents([.medium, .fraction(0.25)])
+                .presentationDragIndicator(.visible)
+                .interactiveDismissDisabled()
+                .onAppear {
+                    guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                          let controller = windowScene.windows.first?.rootViewController?.presentedViewController else {
+                        return
+                    }
+                    controller.view.backgroundColor = .clear
+                }
+        }
+        .onChange(of: vm.scanType) { _ in vm.recognizedItems = [] }
+        .onChange(of: vm.textContentType) { _ in vm.recognizedItems = [] }
+        .onChange(of: vm.recognizesMultipleItems) { _ in vm.recognizedItems = []}
+    }
+    
+    private var headerView: some View {
+        VStack {
+            HStack {
+                Picker("Scan Type", selection: $vm.scanType) {
+                    Text("Barcode").tag(ScanType.barcode)
+                }.pickerStyle(.segmented)
+                
+                Toggle("Scan multiple", isOn: $vm.recognizesMultipleItems)
+            }.padding(.top)
+            
+            Text(vm.headerText).padding(.top)
+        }.padding(.horizontal)
+    }
+    
+    private var bottomContainerView: some View {
+        VStack {
+            headerView
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 16) {
+                    ForEach(vm.recognizedItems) { item in
+                        switch item {
+                        case .barcode(let barcode):
+                            Text(barcode.payloadStringValue ?? "Unknown barcode")
+                            
+                        case .text(let text):
+                            Text(text.transcript)
+                            
+                        @unknown default:
+                            Text("Unknown")
+                        }
+                    }
+                }
+                .padding()
             }
         }
-    }
-    
-    
-    struct FoodAllergyView_Previews: PreviewProvider {
-        static var previews: some View {
-            BarcodeView(allergyListViewModel: AllergyListViewModel())
-        }
-    }
-    
-    private func addAllergy() {
-        // 1
-        let allergy = OldFood(id: "question", imageName: "iName", name: "name")
-        // 2
-        allergyListViewModel.add(allergy)
-        // 3
     }
 }
 
 
+
+
+ /*struct FoodAllergyView_Previews: PreviewProvider {
+     static var previews: some View {
+         BarcodeView()
+     }
+ }
+ */
