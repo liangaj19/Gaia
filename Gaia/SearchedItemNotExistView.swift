a4c0b459d907e19da8e83326d8be1f8bf7346e43
Anna Mei <amcode16@gmail.com>
2022-12-13 00:20:56 -0600

    edits

--- Gaia/SearchedItemNotExistView.swift
+++ Gaia/SearchedItemNotExistView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct SearchedItemNotExistView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("Item Not Found")
+                    .font(.system(size: 50))
+                    .fontWeight(.bold)
+                    .fontWeight(.bold)
+                    .padding(.top, 100)
+                    .padding(.bottom, 40)
+                    .padding(.leading, 50)
+                    .padding(.trailing, 50)
+                    .frame(maxWidth: .infinity, alignment:.center)
+                    .background(Color("pearlyPurple"))
+                    .foregroundColor(Color.white)
+                    .mask(RoundedRectangle(cornerRadius: 30))
+                Text("Sorry, this product does not exist in the database")
+                    //.padding(.top, 40)
+                    .foregroundColor(Color.black)
+                    .font(.system(size: 30))
+                    //.fontWeight(.light)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            
+        }
+        .ignoresSafeArea()
     }
 }
 
